5eae8426bb08b676872c6808a6d6bf165e1e7588
Alain O'Dea <alain.odea@gmail.com>
2014-04-10 00:23:44 +0000

    Prepare for ghc-builders

--- bin/tomcat01.sh
+++ bin/tomcat01.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 ssh root@smartos03 'vmadm create | cut -c25- | xargs -n1 vmadm reboot' < machines/tomcat01.json
-ssh root@tomcat01 '
-pkgin -y update
-pkgin -y upgrade
-pkgin -y install apache-tomcat
-'
+
--- bin/update_dns.sh
+++ bin/update_dns.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 scp -r dns/* root@ns1:/
-#scp -r dns/* root@ns2:/
+scp -r dns/* root@ns2:/
 ssh root@ns1 "svcadm refresh dns/server"
-#ssh root@ns2 "svcadm refresh dns/server"
+ssh root@ns2 "svcadm refresh svc:/pkgsrc/bind:default"
 scp -r ns1/* root@ns1:/
-#scp -r ns2/* root@ns2:/
+scp -r ns2/* root@ns2:/
 ssh root@ns1 "svcadm refresh dhcpd"
-#ssh root@ns1 "svcadm refresh dhcpd"
+ssh root@ns2 "svcadm refresh svc:/pkgsrc/isc-dhcpd:default"
