9de3e35ea2f3c7bf7db6b1c94fc7094fb132ecda
Alain O'Dea <AlainODea@users.noreply.github.com>
2015-10-17 22:32:40 -0230

    Clean up DNS and DNS update

--- bin/update_dns.sh
+++ bin/update_dns.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 scp -r dns/* root@ns1:/
-scp -r dns/* root@ns2:/
-ssh root@ns1 "svcadm refresh dns/server"
-ssh root@ns2 "svcadm refresh svc:/pkgsrc/bind:default"
 scp -r ns1/* root@ns1:/
+ssh root@ns1 '
+  svcadm refresh svc:/pkgsrc/bind:default
+  svcadm restart svc:/pkgsrc/isc-dhcpd:default
+'
+scp -r dns/* root@ns2:/
 scp -r ns2/* root@ns2:/
-ssh root@ns1 "svcadm restart dhcpd"
-ssh root@ns2 "svcadm restart svc:/pkgsrc/isc-dhcpd:default"
+ssh root@ns2 '
+  svcadm refresh svc:/pkgsrc/bind:default
+  svcadm restart svc:/pkgsrc/isc-dhcpd:default
+'
